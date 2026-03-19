723c08feec0803600d4cfb8831b1041939a4b3ac
ctalkobt@ctalkobt.net <ctalkobt@ctalkobt.net>
2026-03-19 15:48:56 -0400

    Profiler Pane

--- src/gui/pane_profiler.h
+++ src/gui/pane_profiler.h
@@ -3,6 +3,9 @@
 
 #include "pane_base.h"
 #include <wx/bitmap.h>
+#include <wx/notebook.h>
+#include <wx/listctrl.h>
+#include <wx/scrolwin.h>
 
 class PaneProfiler : public SimPane {
 public:
@@ -14,10 +17,38 @@ public:
 private:
     void OnPaint(wxPaintEvent& event);
     void OnMouseMove(wxMouseEvent& event);
+    void OnLeftClick(wxMouseEvent& event);
+    void OnRightClick(wxMouseEvent& event);
     void OnClear(wxCommandEvent& event);
+    void OnSaveImage(wxCommandEvent& event);
+    void OnColumnClick(wxListEvent& event);
+    void OnListItemActivated(wxListEvent& event);
+    void OnZoomChange(wxCommandEvent& event);
+    void OnCanvasSize(wxSizeEvent& event);
+
+    uint16_t GetAddressAtPos(int x, int y);
+    int      GetDrawSize();
+    void UpdateList();
+    void UpdateCanvasScroll();
 
     wxBitmap m_bitmap;
+    wxImage  m_image;
     uint32_t m_maxHits;
+    wxScrolledWindow* m_canvas;
+    wxNotebook* m_notebook;
+    wxListCtrl* m_list;
+    wxStaticText* m_statusText;
+    wxSlider*     m_zoomSlider;
+
+    struct ProfilerItem {
+        uint16_t addr;
+        uint32_t hits;
+        uint32_t cycles;
+    };
+    std::vector<ProfilerItem> m_items;
+    int m_sortCol;
+    bool m_sortAsc;
+    int  m_zoom;
 };
 
 #endif
